72b46b0e67035150bc57a5147f4222a6b6de676c
Marco Seraphin <seraphin@develope-it.de>
2023-01-09 15:32:31 +0100

    added gradio demo folder structure

--- PyTorchVideoCourse/Deployment/deployment.py
+++ PyTorchVideoCourse/Deployment/deployment.py
@@ -175,23 +175,23 @@ print(f"example_list: {example_list}")
 # -------------------------------
 
 # Create title, description and article strings
-# title = "FoodVision Mini 🍕🥩🍣"
-# description = "An EfficientNetB2 feature extractor computer vision model to classify images of food as pizza, steak or sushi."
-# article = "Created at [09. PyTorch Model Deployment](https://www.learnpytorch.io/09_pytorch_model_deployment/)."
+title = "FoodVision Mini 🍕🥩🍣"
+description = "An EfficientNetB2 feature extractor computer vision model to classify images of food as pizza, steak or sushi."
+article = "Created at [09. PyTorch Model Deployment](https://www.learnpytorch.io/09_pytorch_model_deployment/)."
 
-# # Create the Gradio demo
-# demo = gr.Interface(fn=predict, # mapping function from input to output
-#                     inputs=gr.Image(type="pil"), # what are the inputs?
-#                     outputs=[gr.Label(num_top_classes=3, label="Predictions"), # what are the outputs?
-#                              gr.Number(label="Prediction time (s)")], # our fn has two outputs, therefore we have two outputs
-#                     examples=example_list, 
-#                     title=title,
-#                     description=description,
-#                     article=article)
+# Create the Gradio demo
+demo = gr.Interface(fn=predict, # mapping function from input to output
+                    inputs=gr.Image(type="pil"), # what are the inputs?
+                    outputs=[gr.Label(num_top_classes=3, label="Predictions"), # what are the outputs?
+                             gr.Number(label="Prediction time (s)")], # our fn has two outputs, therefore we have two outputs
+                    examples=example_list, 
+                    title=title,
+                    description=description,
+                    article=article)
 
-# # Launch the demo!
-# demo.launch(debug=False, # print errors locally?
-#             share=True) # generate a publically shareable URL?
+# Launch the demo!
+demo.launch(debug=False, # print errors locally?
+            share=True) # generate a publically shareable URL?
 
 
 # Create FoodVision mini demo path
