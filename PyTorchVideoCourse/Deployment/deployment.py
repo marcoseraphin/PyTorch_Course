abf756efa628bc7eddad9f70a353700199456a3e
Marco Seraphin <seraphin@develope-it.de>
2023-01-09 14:15:56 +0100

    added gradio

--- PyTorchVideoCourse/Deployment/deployment.py
+++ PyTorchVideoCourse/Deployment/deployment.py
@@ -1,20 +1,30 @@
 import torch
 import torchvision
 import utils
+import gradio as gr
 import data_setup, engine
+from typing import Tuple, Dict
+import predict
+from timeit import default_timer as timer 
 from torchinfo import summary
 import matplotlib.pyplot as plt
 from pathlib import Path
 from torch import nn
+import random
+import os
+from PIL import Image
 import download_data, set_seed, helper_functions
 
 # Set the device      
 device = "mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Using device: {device}")
 
+# if os.path.isfile('models/09_pretrained_effnetb2_feature_extractor_pizza_steak_sushi.pth'):
+#     effnetb2_model = torch.load('models/09_pretrained_effnetb2_feature_extractor_pizza_steak_sushi.pth')
+
 # Load data
 image_path = download_data.download_data(source="https://github.com/marcoseraphin/PyTorch_Course/raw/main/PyTorchVideoCourse/ZipData/pizza_steak_sushi.zip",
-                                         destination="pizza_steak_sushi")
+                                        destination="pizza_steak_sushi")
 
 # Setup directories
 train_dir = image_path / "train"
@@ -37,7 +47,7 @@ for param in effnetb2_model.parameters():
 effnetb2_model.classifier = nn.Sequential(
     nn.Dropout(p=0.3, inplace=True), # keep dropout layer same
     nn.Linear(in_features=1408, # keep in_features same 
-              out_features=3)) # change out_features to suit our number of classes
+            out_features=3)) # change out_features to suit our number of classes
 
 # Since we're creating a new layer with random weights (torch.nn.Linear), 
 # let's set the seeds
@@ -54,13 +64,13 @@ summary(effnetb2_model,
 
 # Setup DataLoaders
 train_dataloader_effnetb2, test_dataloader_effnetb2, class_names = data_setup.create_dataloaders(train_dir=train_dir,
-                                                                                                 test_dir=test_dir,
-                                                                                                 transform=effnetb2_transforms,
-                                                                                                 batch_size=32)
+                                                                                                test_dir=test_dir,
+                                                                                                transform=effnetb2_transforms,
+                                                                                                batch_size=32)
 
 # Setup optimizer
 optimizer = torch.optim.Adam(params=effnetb2_model.parameters(),
-                             lr=1e-3)
+                            lr=1e-3)
 # Setup loss function
 loss_fn = torch.nn.CrossEntropyLoss()
 
@@ -80,10 +90,103 @@ plt.show()
 
 # Save the model
 utils.save_model(model=effnetb2_model,
-                 target_dir="models",
-                 model_name="09_pretrained_effnetb2_feature_extractor_pizza_steak_sushi.pth")
+                    target_dir="models",
+                    model_name="09_pretrained_effnetb2_feature_extractor_pizza_steak_sushi.pth")
 
 
 # Get the model size in bytes then convert to megabytes
 pretrained_effnetb2_model_size = Path("models/09_pretrained_effnetb2_feature_extractor_pizza_steak_sushi.pth").stat().st_size // (1024*1024) # division converts bytes to megabytes (roughly) 
 print(f"Pretrained EffNetB2 feature extractor model size: {pretrained_effnetb2_model_size} MB")
+
+# Count number of parameters in EffNetB2
+effnetb2_total_params = sum(torch.numel(param) for param in effnetb2_model.parameters())
+
+# Create a dictionary with EffNetB2 statistics
+effnetb2_stats = {"test_loss": effnetb2_results["test_loss"][-1],
+                  "test_acc": effnetb2_results["test_acc"][-1],
+                  "number_of_parameters": effnetb2_total_params,
+                  "model_size (MB)": pretrained_effnetb2_model_size}
+
+print(f"Statistic for EffNetB2: {effnetb2_stats}")
+
+print(f"Gradio version: {gr.__version__}")
+
+
+def predict(img) -> Tuple[Dict, float]:
+    """Transforms and performs a prediction on img and returns prediction and time taken.
+    """
+    # Start the timer
+    start_time = timer()
+    
+    # Transform the target image and add a batch dimension
+    img = effnetb2_transforms(img).unsqueeze(0)
+    
+    # Put model into evaluation mode and turn on inference mode
+    effnetb2_model.eval()
+    with torch.inference_mode():
+        # Pass the transformed image through the model and turn the prediction logits into prediction probabilities
+        pred_probs = torch.softmax(effnetb2_model(img), dim=1)
+    
+    # Create a prediction label and prediction probability dictionary for each prediction class (this is the required format for Gradio's output parameter)
+    pred_labels_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))}
+    
+    # Calculate the prediction time
+    pred_time = round(timer() - start_time, 5)
+    
+    # Return the prediction dictionary and prediction time 
+    return pred_labels_and_probs, pred_time
+
+
+# Put EffNetB2 on CPU
+effnetb2_model.to("cpu") # Gradio COST extra money for using the GPU
+
+# Check the device
+next(iter(effnetb2_model.parameters())).device
+
+# Get a list of all test image filepaths
+test_data_paths = list(Path(test_dir).glob("*/*.jpg"))
+
+# Randomly select a test image path
+random_image_path = random.sample(test_data_paths, k=1)[0]
+
+# Open the target image
+image = Image.open(random_image_path)
+print(f"[INFO] Predicting on image at path: {random_image_path}\n")
+
+# Predict on the target image and print out the outputs
+# pred_dict, pred_time = predict(img=image,
+#                                        model=effnetb2_model,
+#                                        transform=effnetb2_transforms,
+#                                        class_names=class_names)
+
+pred_dict, pred_time = predict(img=image)
+
+
+print(f"Prediction label and probability dictionary: \n{pred_dict}")
+print(f"Prediction time: {pred_time} seconds")
+
+# Create a list of example inputs to our Gradio demo
+example_list = [[str(filepath)] for filepath in random.sample(test_data_paths, k=3)]
+print(f"example_list: {example_list}")
+
+# Gradio interface for deployment
+# -------------------------------
+
+# Create title, description and article strings
+title = "FoodVision Mini 🍕🥩🍣"
+description = "An EfficientNetB2 feature extractor computer vision model to classify images of food as pizza, steak or sushi."
+article = "Created at [09. PyTorch Model Deployment](https://www.learnpytorch.io/09_pytorch_model_deployment/)."
+
+# Create the Gradio demo
+demo = gr.Interface(fn=predict, # mapping function from input to output
+                    inputs=gr.Image(type="pil"), # what are the inputs?
+                    outputs=[gr.Label(num_top_classes=3, label="Predictions"), # what are the outputs?
+                             gr.Number(label="Prediction time (s)")], # our fn has two outputs, therefore we have two outputs
+                    examples=example_list, 
+                    title=title,
+                    description=description,
+                    article=article)
+
+# Launch the demo!
+demo.launch(debug=False, # print errors locally?
+            share=True) # generate a publically shareable URL?
