233434bb144498f11011336ecff5a71151457453
Marco Seraphin <seraphin@develope-it.de>
2023-03-07 13:18:00 +0100

    updated Open AI client

--- OpenAI_Sample.py
+++ OpenAI_Sample.py
@@ -6,13 +6,12 @@ from dotenv import load_dotenv
 load_dotenv('TOKEN.env')  # take environment variables from .env.
 
 OPEN_AI_API_KEY=os.getenv('OPEN_AI_API_KEY')
-
-openai.api_key = os.getenv('OPEN_AI_API_KEY')
+openai.api_key=OPEN_AI_API_KEY
 
 # Modelname see => https://platform.openai.com/docs/models/gpt-3-5
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Schreibe einen Bericht über Motorräder",
+  prompt="Welcher Laptop wäre ideal für Office Arbeiten in Bezug auf Speicher und Chipsatz",
   temperature=0.4,
   max_tokens=1443,
   top_p=1,
@@ -21,3 +20,4 @@ response = openai.Completion.create(
 )
 
 print(f"Response = {response}")
+print(f"Text = {response.choices[0].text}")
